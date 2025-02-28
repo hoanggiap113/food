dda6934cb41edfcbf68a654e947198f30ab8348f
hoanggiap113 <hoangnguyengiap04@gmail.com>
2025-02-28 14:43:21 +0700

    update project

---
+++ src/main/java/com/food/gei/GeiApplication.java
@@ -0,0 +1,13 @@
+package com.food.gei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/gei/GeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.gei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
